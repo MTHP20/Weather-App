4b759b15b69b8ca9d9ca6f3408a463004df65237
Lisubu Rai (2020) ZJAC272 <ZJAC272@live.rhul.ac.uk>
2024-09-10 18:51:09 +0100

    2

--- weather.py
+++ weather.py
@@ -1,4 +1,13 @@
 import requests 
+from dotenv import load_dotenv
+import os
 
-def get_lan_lon():
-    resp = requests.get('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}')
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+def get_lan_lon(city_name,state_code,country_code,API_key):
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}')
+
+    print(resp)
+
+get_lan_lon('Toronto', 'ON', 'Canada', api_key)
