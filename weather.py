d3bf866f82fdc5a7b8984dd4323955b5fd652532
Lisubu Rai (2020) ZJAC272 <ZJAC272@live.rhul.ac.uk>
2024-09-10 19:18:54 +0100

    Updated get weather function to get specific data from API

--- weather.py
+++ weather.py
@@ -1,9 +1,17 @@
 import requests 
 from dotenv import load_dotenv
 import os
+from dataclasses import dataclass
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
+
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: float
 
 def get_lan_lon(city_name,state_code,country_code,API_key):
     resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
@@ -12,6 +20,18 @@ def get_lan_lon(city_name,state_code,country_code,API_key):
     return lat, lon
     
 def get_current_weather(lat, lon, API_key):
-    resp = requests.get('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}')
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
+    data = WeatherData(
+        main = resp.get('weather')[0].get('main'),
+        description = resp.get('weather')[0].get('description'),
+        icon = resp.get('weather')[0].get('icon'),
+        temperature = resp.get('main')[0].get('temp')
+    )
+    
+    return data
 
-print(get_lan_lon('Toronto', 'ON', 'Canada', api_key))
+if __name__ == '__main__':
+    lat, lon = get_lan_lon('Toronto', 'ON', 'Canada', api_key)
+    get_current_weather(lat,lon, api_key)
+    
+    
