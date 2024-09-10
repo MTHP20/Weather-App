a227b3375f36a1d57d3039fe00f50697465c554d
Lisubu Rai (2020) ZJAC272 <ZJAC272@live.rhul.ac.uk>
2024-09-10 19:00:36 +0100

    Complete lan, long function to get the specified data

--- weather.py
+++ weather.py
@@ -6,8 +6,11 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 
 def get_lan_lon(city_name,state_code,country_code,API_key):
-    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}')
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+    data = resp[0]
+    lat, lon = data.get("lat"), data.get("lon")
+    return lat, lon
+    
 
-    print(resp)
 
-get_lan_lon('Toronto', 'ON', 'Canada', api_key)
+print(get_lan_lon('Toronto', 'ON', 'Canada', api_key))
